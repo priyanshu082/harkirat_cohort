bf2ec32948355c57a1428f6af5e03d50f069c69c
priyanshu082 <priyanshusingh216@gmail.com>
2024-10-29 11:21:48 +0530

    WEEK 7 :: RECOIL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,58 @@
 import React, { lazy, Suspense, useContext, useState } from 'react'
-import { CountContext } from './assingment/week-7/contextAPI/Context';
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import  {countAtom}  from './assingment/week-7/recoil/store/atoms/count.jsx'
+// import { CountContext } from './assingment/week-7/contextAPI/Context';
 
 
 function App() {
 
-const [count , setCount]=useState(0);
-
   return (
-    <div>
-  <CountContext.Provider value={count}>
-    <Count setCount={setCount}/>
-  </CountContext.Provider>
-    </div>
+
+<RecoilRoot>
+<Count />
+</RecoilRoot>
+  
+
+
   )
 }
 
-const Count=({setCount})=>{
-  
+const Count=()=>{
+console.log("re render count")
   return <div>
-     <CountReRender/>
-     <Buttons setCount={setCount}/>
+
+    <div><CountReRender/></div> 
+     <Buttons />
+     <div><EventCountReRender/></div>
   </div>
 }
 
 const CountReRender=()=>{
-  const count =useContext(CountContext)
+const count=useRecoilValue(countAtom)
+
+console.log("re render countrerender")
   return <>
   {count}
   </>
 }
 
-const Buttons =({setCount})=>{
-  const count =useContext(CountContext)
+const EventCountReRender=()=>{
+  const count =useRecoilValue(countAtom)
+  console.log("re render odd even")
+  return <div>
+    {(count%2===0) ?"Even" : "Odd"}
+  </div>
+}
+
+const Buttons =({})=>{
+// const [count,setCount]=useRecoilState(countAtom)
+const setCount=useSetRecoilState(countAtom)
+console.log("re render buttons")
   return <>
-  <button onClick={()=>{setCount(prev=>prev+1)}}>
+  <button onClick={()=>setCount(prev=>prev+1)}>
     Increase
   </button>
-  <button onClick={()=>{setCount(prev=>prev-1)}}>
+  <button onClick={()=>setCount(prev=>prev-1)}>
     Decrease 
   </button>
   </>
