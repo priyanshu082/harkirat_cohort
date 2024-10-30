d9d7703d09b0671d99831922109f02d42f8d274e
priyanshu082 <priyanshusingh216@gmail.com>
2024-10-30 12:13:57 +0530

    WEEK 7 :: RECOIL ATOMFAMILY AND SELECTORFAMILY

--- frontend/src/assingment/week-7/recoil/assingment/store/atoms/todo.jsx
+++ frontend/src/assingment/week-7/recoil/assingment/store/atoms/todo.jsx
@@ -1,10 +1,33 @@
-import { atom, selector } from "recoil";
+import { atom, atomFamily, selector, selectorFamily } from "recoil";
 import axios from "axios"
 
 export const items=atom({
     key:"items",
     default:[]
+    //doing in this way has its downside as if one of todo changes all the todos will change cuasing the re-render 
 })
+
+//so we use atomfamily for this 
+//there is a concept of atom family here 
+export const todoFamily=atomFamily({
+    key:"todoFamily",
+    default:id=>{}
+    //filter the specify todo with specific id 
+    //this family create a separate atom for every component and return the atom and due this only particular component re render not all components
+})  
+
+//for getting data from backend and use it we use selectorFamily
+export const todoFamily2=atomFamily({
+    key:"todoFamily2",
+    default:selectorFamily({
+        key:"todoSelectorFamily",
+        get: (id)=>async({get})=>{
+            const res = await axios.get("api here ")
+            return //the data here
+        }
+    })
+})
+
 
 export const item=atom({
     key:"item",
@@ -21,11 +44,11 @@ export const searchQuery=atom({
 
 export const ItemSelector=selector({
     key:"searchResults",
-    get:({get})=>{
-        const todos=get(items)
-        const query=get(searchQuery).toLowerCase();
-        if(!query) return todos;
-        return todos.filter((todo)=>todo.name.toLowerCase().includes(query))
+    get:function(id){
+        return async function ({get}){
+            const res= await axios.get("apis here "+ id)
+            return res.data.data
+        }
     }
 })
 
@@ -45,3 +68,5 @@ export const notifications=atom({
 })
 //we can use selectore to incorporate the async data in recoil
 //this will automatically generate the structure of data and store it in the form of obejct and you can access them in from end 
+
+
