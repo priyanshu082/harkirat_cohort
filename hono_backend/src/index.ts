556b9e1d208627cb99005c43e46b4dab3e7ab769
priyanshu082 <priyanshusingh216@gmail.com>
2024-11-05 13:34:43 +0530

    Created WEEK-9-TS

--- hono_backend/src/index.ts
+++ hono_backend/src/index.ts
@@ -2,8 +2,29 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-//we can get both request and resposne from this "c"
-app.get('/', (c) => {
+//for writting the middle ware
+async function authMiddleware(c:any,next:any){
+  if(c.req.header("Authorization")){
+    console.log("before")
+    await next();
+    console.log("after")
+  } else{
+    console.log("before") 
+    await next();
+    console.log("after")
+    return c.text("You dont have acess")
+  }
+}
+
+// app.use(authMiddleware)
+
+app.get('/',authMiddleware, async (c) => {
+  // const body = await c.req.json()
+  // console.log("C----->>>>>",c.req.query("param"));
+  // console.log("C----->>>>>",body);
+  // console.log("Headers------>>>>>",c.req.header("Authorization"));
+  // console.log("Methods------>>>>>",c.method);
+  // console.log("URL------>>>>>",c.req.query("param"));
   return c.text('Hello Hono!')
 })
 
--- week-9ts/src/arrays.ts
+++
@@ -1,9 +0,0 @@
-function maxNum (arr:number[]){
-    let max=0;
-    for(let i=0;i<arr.length;i++) if(arr[i]>max) max=arr[i]
-    return max;
-}
-
-const num:number[]=[1,2,3,4,5,6,7]
-
-console.log(maxNum(num))
