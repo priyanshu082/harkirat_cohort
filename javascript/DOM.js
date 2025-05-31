62670f38d55c0f3bedecb4fce929882396b44e91
priyanshu082 <priyanshusingh216@gmail.com>
2025-05-31 08:46:49 +0530

    SOME CHANGES

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -18,8 +18,10 @@ function addEvent2(){
     })
 }
 
-addEvent()
-addEvent2()
+
+
+// addEvent()
+// addEvent2()
 
 // What Happens Step-by-Step:
 //
@@ -37,6 +39,54 @@ addEvent2()
 //    - The click event listener (which was added earlier) runs.
 //    - The listener has access to 'counter' because of JavaScript closure.
 //    - It increments and logs the value of 'counter' each time the button is clicked.
+
+
+
+//garbage collections and removeEventlisteners
+
+function removeEvent(){
+    document.getElementById("btn").removeEventListener("click",()=>{
+        console.log("button clicked ")
+    })
+}
+//since eventlistners are heavy which can take too much space removing them is a good thing
+//but if you remove them before they are called it will throw an error
+//so you should remove them after they are called
+
+
+// Build a live character counter
+// When a user types into a textarea, show how many characters are remaining (max 100).
+function maxLen(){
+    // let counter=100;
+    let inputarea=document.getElementById("txtarea")
+    let counter=document.getElementById("counter")
+    inputarea.addEventListener("input",()=>{
+        counter.textContent=100-inputarea.value.length+"characters remaining" 
+    })
+}
+maxLen();
+
+
+
+
+//Create a debounce-enabled search bar
+function deBounce(){
+    let timer;
+    let searchInput=document.getElementById("debounce")
+    let searchTxt=document.getElementById("searchtxt")
+    searchInput.addEventListener("input",()=>{
+        clearTimeout(timer)
+        timer=setTimeout(() => {
+            console.log("Searching:", searchInput.value);
+            searchTxt.textContent = "Searching for: " + searchInput.value;
+        }, 500);
+    })
+}
+
+deBounce()
+
+
+
 
 
 
